b9bd3d555b39470197cf67e1d619e0bddd647baa
EthanHunt-668 <pranayanand668@gmail.com>
2024-09-13 23:00:10 +0530

    first commit

---
+++ src/test/java/io/leucine/CollegeDirectory/CollegeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.leucine.CollegeDirectory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
